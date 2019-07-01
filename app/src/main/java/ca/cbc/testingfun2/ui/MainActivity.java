750c32cf24f6a6a3cc9db7ed963f1b1304552720
Nick Rose <nicholas.rose@cbc.ca>
2019-07-01 15:37:49 -0400

    Convert MainActivity to java to test interoperability of kotlin coroutines with java

---
+++ app/src/main/java/ca/cbc/testingfun2/ui/MainActivity.java
@@ -0,0 +1,72 @@
+package ca.cbc.testingfun2.ui;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
+
+import javax.inject.Inject;
+
+import ca.cbc.testingfun2.R;
+import ca.cbc.testingfun2.data.GitHubJob;
+import ca.cbc.testingfun2.util.Resource;
+import ca.cbc.testingfun2.vm.GitHubJobsViewModel;
+import ca.cbc.testingfun2.vm.ViewModelFactory;
+import dagger.android.support.DaggerAppCompatActivity;
+
+public class MainActivity extends DaggerAppCompatActivity {
+
+    @Inject
+    ViewModelFactory viewModelFactory;
+
+    private GitHubJobsViewModel viewModel;
+    private GitHubJobsAdapter adapter = new GitHubJobsAdapter();
+    private int id = 666;
+
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(GitHubJobsViewModel.class);
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setAdapter(adapter);
+        progressBar = findViewById(R.id.progress_bar);
+
+        observeGitHubJobs();
+        viewModel.fetchGitHubJobs();
+
+        findViewById(R.id.button).setOnClickListener(__ -> {
+            GitHubJob gitHubJob = new GitHubJob(String.valueOf(id++), "A title", "A company");
+            viewModel.insertJob(gitHubJob);
+        });
+
+        findViewById(R.id.refresh).setOnClickListener(__ -> viewModel.fetchGitHubJobs());
+    }
+
+    private void observeGitHubJobs() {
+        viewModel.getGitHubJobs().observe(this, resource -> {
+            if (resource instanceof Resource.Loading) {
+                progressBar.setVisibility(View.VISIBLE);
+                if (resource.getData() != null) {
+                    adapter.submitList(resource.getData());
+                }
+            } else if (resource instanceof Resource.Success) {
+                progressBar.setVisibility(View.GONE);
+                adapter.submitList(resource.getData());
+            } else if (resource instanceof Resource.Error) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(this, "Failed to fetch new Jobs", Toast.LENGTH_LONG)
+                        .show();
+                adapter.submitList(resource.getData());
+            }
+        });
+    }
+}
